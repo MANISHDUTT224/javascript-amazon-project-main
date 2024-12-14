db36f17bbf303480a87c07e817175263ced95237
manish <manish.sunildutt@gmail.com>
2024-12-14 16:25:18 +0530

    fix price formatting and delivery options

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart,addtocart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatmoney } from './utils/money.js';
 let productHTML='',html;
 products.forEach((product)=>{
     
@@ -23,7 +24,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${((product.priceCents)/100).toPrecision(4)}
+            $${formatmoney(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ javascript-amazon-project-main/scripts/utils/money.js
@@ -0,0 +1,3 @@
+export const formatmoney=(priceCents)=>{
+    return (priceCents/100).toFixed(2);
+}
