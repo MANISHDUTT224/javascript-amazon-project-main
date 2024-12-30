4e6b9bca81436593010a842750406a7c0772e3db
manish <manish.sunildutt@gmail.com>
2024-12-30 15:25:40 +0530

    just logo changes

---
+++ javascript-amazon-project-main/data/backend-practice.js
@@ -0,0 +1,4 @@
+const xhr=new XMLHttpRequest();
+let url="https://manishdutt224.github.io/javascript-amazon-project-main/javascript-amazon-project-main/";
+xhr.open('GET',url);
+xhr.send();
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart,addtocart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import '../data/backend-practice.js'
 let productHTML='',html;
 products.forEach((product)=>{
     
